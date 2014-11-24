4a255556093885533624feb800f9c316884b0b9d
Francois Barre <francois.barre@arondor.com>
2014-11-24 09:20:08 +0100

    codestyle

--- src/include/Xemeiah/kern/format/blob.h
+++ src/include/Xemeiah/kern/format/blob.h
@@ -9,22 +9,22 @@
 
 namespace Xem
 {
-  /**
-   * Blob format is based on SKMap
-   * - this allows for fast finding of the beginning of a blob piece
-   * - piece size is computed using the next iterator size
-   */
-  struct BlobHeader
-  {
-    SKMapHeader skMapHeader;
-    __ui64 totalSize;
-    AllocationProfile pieceAllocationProfile;
-    LocalKeyId mimeTypeId;
-    LocalKeyId mimeSubTypeId;
-    /*
-     * BranchRevId lastUpdatedBranchRevId;
+    /**
+     * Blob format is based on SKMap
+     * - this allows for fast finding of the beginning of a blob piece
+     * - piece size is computed using the next iterator size
      */
-  };
-};
+    struct BlobHeader
+    {
+        SKMapHeader skMapHeader;
+        __ui64 totalSize;
+        AllocationProfile pieceAllocationProfile;
+        LocalKeyId mimeTypeId;
+        LocalKeyId mimeSubTypeId;
+        /*
+         * BranchRevId lastUpdatedBranchRevId;
+         */
+    };
+}
 
 #endif // __XEM_KERN_FORMAT_BLOB_H
